bddb34069621ac8427796d84f6de31a524027e8e
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-25 11:48:53 +0200

    Integrate Nokogiri and letter_count method

--- app/serializers/letter_serializer.rb
+++ app/serializers/letter_serializer.rb
@@ -1,15 +1,18 @@
 class LetterSerializer < ActiveModel::Serializer
   attributes :current_date, :letter_count
 
+  require 'open-uri'
+
   def current_date
     date = Time.now.strftime("%Y-%m-%d")
   end
 
   def letter_count
-    if (@instance_options[:queryy]).length==1
-      "".count(@instance_options[:queryy])
-    else
+    html = Nokogiri::HTML(open 'https://www.espn.com/')
+    html.css('script').remove
+    text = html.at('body').inner_text
+    text.delete!("\n")
+  else
       "pass a  single letter"
-    end
   end
 end
