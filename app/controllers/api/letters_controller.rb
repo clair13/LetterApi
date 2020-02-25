3e8e916ece46356957e79f251318dd922a034f4d
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-25 11:41:32 +0200

    Pass data to Serializer with instance_options

--- app/controllers/api/letters_controller.rb
+++ app/controllers/api/letters_controller.rb
@@ -2,6 +2,6 @@ class Api::LettersController < ApplicationController
 
   def  index
     @letter = params[:query]
-    render json: @letter , serializer: LetterSerializer
+    render json: @letter , serializer: LetterSerializer, queryy: params[:query]
   end
 end
--- app/serializers/letter_serializer.rb
+++ app/serializers/letter_serializer.rb
@@ -1,7 +1,15 @@
 class LetterSerializer < ActiveModel::Serializer
-  attributes :current_date
+  attributes :current_date, :letter_count
 
   def current_date
     date = Time.now.strftime("%Y-%m-%d")
+  end
+
+  def letter_count
+    if (@instance_options[:queryy]).length==1
+      "".count(@instance_options[:queryy])
+    else
+      "pass a  single letter"
+    end
   end
 end
