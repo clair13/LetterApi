c5a3152d6d45f3014c04514e1a2fd84b5ab231c2
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-27 07:40:25 +0200

    Implement method to show first sentence of wiki

--- app/serializers/letter_serializer.rb
+++ app/serializers/letter_serializer.rb
@@ -1,5 +1,5 @@
 class LetterSerializer < ActiveModel::Serializer
-  attributes :current_date, :letter_count
+  attributes :current_date, :letter_count, :query
 
   require 'open-uri'
 
@@ -8,13 +8,24 @@ class LetterSerializer < ActiveModel::Serializer
   end
 
   def letter_count
-    html = Nokogiri::HTML(open 'https://www.espn.com/')
-    html.css('script').remove
-    text = html.at('body').inner_text
-    text.delete!("\n")
+    if (@instant_options[:queryy]).length==1
+      html = Nokogiri::HTML(open 'https://www.espn.com/')
+      html.css('script').remove
+      text = html.at('body').inner_text
+      text.delete!("\n")
 
-    text.count(@instant_options[:queryy])
-  else
+      text.count(@instant_options[:queryy])
+    else
       "pass a  single letter"
   end
+
+  def query
+    if (@instant_options[:query]).length==1
+      given_letter = @instant_options[:queryy]
+      page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{given_letter}"))
+      page.css('script').remove
+      page.css('p').first.text
+    else
+      "No Data here"
+    end
 end
